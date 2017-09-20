fec7680d359f1958c480cb9ff7c368102584cfc2
zhongfeiyang <278371414@qq.com>
2017-09-20 22:01:45 +0800

    head

---
+++ src22.01/app.js
@@ -0,0 +1,6 @@
+//var React=require("react")
+//var ReactDOM=require("react-dom");
+require("./styles/app.scss")
+
+import RootComponent from './scripts/components/RootComponent';
+ReactDOM.render(<RootComponent/>,document.getElementById("app"))
