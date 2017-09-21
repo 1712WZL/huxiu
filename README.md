34b834fd37a2f4f936c3904ca514f892293b1d65
zhongfeiyang <278371414@qq.com>
2017-09-21 10:54:49 +0800

    zhongfeiyang

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 #huxiu
 hhhh
+zhong feiyang
--- src/app.js
+++
@@ -1,6 +0,0 @@
-//var React=require("react")
-//var ReactDOM=require("react-dom");
-require("./styles/app.scss")
-
-import RootComponent from './scripts/components/RootComponent';
-ReactDOM.render(<RootComponent/>,document.getElementById("app"))
